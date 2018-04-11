7d7f37fc10010436d287b0838d31b35779fdbf49
hectormonrealvela <37367485+hectormonrealvela@users.noreply.github.com>
2018-04-11 11:59:37 +0200

    Añadir comentarios.

--- coordenadas_matplotlib.py
+++ coordenadas_matplotlib.py
@@ -1,3 +1,5 @@
+###########   Librerias
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -8,7 +10,8 @@ from mpldatacursor import datacursor
 fig = plt.figure()
 ax =Axes3D(fig)
 
-# Make data.
+###########  Datos figura 3d
+
 X = np.arange(-5, 5, 0.25)
 Y = np.arange(-5, 5, 0.25)
 X, Y = np.meshgrid(X, Y)
@@ -17,10 +20,12 @@ Z = np.sin(R)
 plt.axis("off")
 
 
-# Plot the surface.
+#### Nos da las coordenadas
+
 surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
-
 datacursor(surf)
+
+####### Muestra la figura
 
 plt.show()
