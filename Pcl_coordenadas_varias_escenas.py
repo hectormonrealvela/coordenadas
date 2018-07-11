7696b5322130ab823e29bbc72f742990e1b4a6d2
hectormonrealvela <37367485+hectormonrealvela@users.noreply.github.com>
2018-07-11 10:01:30 +0200

    Simplificar codigo

--- Pcl_coordenadas_varias_escenas.py
+++ Pcl_coordenadas_varias_escenas.py
@@ -17,13 +17,8 @@ from tvtk.pyface.scene_editor import SceneEditor
 from traits.api import HasTraits, Instance, Button, on_trait_change
 from traitsui.api import View, Item, HSplit, Group
 
-
-
 cont = -1
 cont2 = 0
-
-
-
 lista =['/home/hector/PycharmProjects/mayavi/mapas/cloud_yaw_frame_number_0.pcd',
             '/home/hector/PycharmProjects/mayavi/mapas/cloud_yaw_frame_number_1.pcd',
             '/home/hector/PycharmProjects/mayavi/mapas/cloud_yaw_frame_number_2.pcd',
@@ -32,7 +27,6 @@ lista =['/home/hector/PycharmProjects/mayavi/mapas/cloud_yaw_frame_number_0.pcd'
             '/home/hector/PycharmProjects/mayavi/mapas/cloud_yaw_frame_number_5.pcd',
             '/home/hector/PycharmProjects/mayavi/mapas/cloud_yaw_frame_number_6.pcd',
             ]
-
 
 
 class MyDialog(HasTraits):
@@ -44,7 +38,22 @@ class MyDialog(HasTraits):
     scene2 = Instance(MlabSceneModel, ())
 
 
+    def draw(self):
+        print ("Actualmente se esta representando Cloud_yaw_frame_number_ %i" % (cont))
+
+        cloud_source = pcl.load(lista[cont])
+        points_array = cloud_source.to_array()
+
+        self.points3d_draw = points3d(points_array[:, 0:1], points_array[:, 1:2], points_array[:, 2:3],
+                                      points_array[:, 2:3], mode='sphere', scale_mode='none',
+                                      scale_factor=self.scale_factor, reset_zoom=False, figure=self.scene2.mayavi_scene)
+
+        points_target = cloud_source.to_array()
+        self.points3d_draw.mlab_source.reset(x=points_target[:, 0:1], y=points_target[:, 1:2], z=points_target[:, 2:3],
+                                             scalars=points_target[:, 2:3], figure=self.scene2.mayavi_scene)
+
     def picker_callback(self,picker):
+
         global cont2
         cont2 +=1
         print
@@ -52,14 +61,17 @@ class MyDialog(HasTraits):
         print picker.pick_position
         print
 
+
         [x, y, z, ] = picker.pick_position
 
-        points3d(x, y, z, color=(0, 0, 0), scale_factor=0.1, mode='sphere', scale_mode='none', reset_zoom=False)
+        self.scene2.mlab.points3d(x, y, z, color=(0, 0, 0), scale_factor=0.1, mode='sphere', scale_mode='none',
+                             reset_zoom=False, figure=self.scene2.mayavi_scene)
 
         with open('Bbdd_CLoud.doc', "a") as file:
             file.write(" BBdd Frame: %i Objeto num: %i  " % (cont,cont2))
             file.write("Coordenada x: %.2f, Coordenada y: %.2f, Coordenada z: %.2f\n\n" % (picker.pick_position))
             file.close()
+
 
     button1 = Button('limpiar pantalla')
     button2 = Button('Siguiente')
@@ -77,58 +89,23 @@ class MyDialog(HasTraits):
         while p<4:
          for child in self.scene2.mayavi_scene.children:
             child.remove()
-
-        p+=1
-            
-        
+         p+=1
 
 
     @on_trait_change('button2')
     def siguiente(self):
         global cont
         cont += 1
+        self.draw()
 
-        print ("Actualmente se esta representando Cloud_yaw_frame_number_ %i"%(cont))
-
-        cloud_source = pcl.load(lista[cont])
-        points_array = cloud_source.to_array()
-
-        self.points3d_draw = points3d(points_array[:, 0:1], points_array[:, 1:2], points_array[:, 2:3],
-                                      points_array[:, 2:3], mode='sphere', scale_mode='none',
-                                      scale_factor=self.scale_factor, reset_zoom=False, figure=self.scene2.mayavi_scene)
-
-        points_target = cloud_source.to_array()
-        self.points3d_draw.mlab_source.reset(x=points_target[:, 0:1], y=points_target[:, 1:2], z=points_target[:, 2:3],
-                                             scalars=points_target[:, 2:3], figure=self.scene2.mayavi_scene)
-
-        cursor3d = mlab.points3d(0., 0., 0., mode='sphere',
-                                 color=(0, 0, 0),
-                                 scale_factor=0.1)
 
     @on_trait_change('button3')
     def anterior(self):
         global cont
         cont -= 1
-        print ("Actualmente se esta representando Cloud_yaw_frame_number_ %i" % (cont))
+        self.draw()
 
-        cloud_source = pcl.load(lista[cont])
-        points_array = cloud_source.to_array()
-
-        self.points3d_draw = points3d(points_array[:, 0:1], points_array[:, 1:2], points_array[:, 2:3],
-                                          points_array[:, 2:3], mode='sphere', scale_mode='none',
-                                          scale_factor=self.scale_factor, reset_zoom=False,
-                                          figure=self.scene2.mayavi_scene)
-
-        points_target = cloud_source.to_array()
-        self.points3d_draw.mlab_source.reset(x=points_target[:, 0:1], y=points_target[:, 1:2],
-                                                 z=points_target[:, 2:3],
-                                                 scalars=points_target[:, 2:3], figure=self.scene2.mayavi_scene)
-
-        cursor3d = mlab.points3d(0., 0., 0., mode='sphere',
-                                     color=(0, 0, 0),
-                                     scale_factor=0.1)
-
-        # The layout of the dialog created
+       
     view = View(HSplit(
 
                   Group(
